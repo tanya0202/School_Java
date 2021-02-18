cb0a7f7a4881d5d04f35e52d7bc8873845434934
Tanya <tanya1527.cse18@chitkara.edu.in>
2021-02-18 14:12:03 +0530

    Add files via upload

---
+++ JavaSchool.js
@@ -0,0 +1,9 @@
+
+document.getElementById(b2).onclick = function(
+//    var  y=document.getElementsByClassName(p2);
+//     var z=document.getElementsByClassName(p1)
+//      document.replaceChild(p2, p1);
+
+var x=document.getElementsByClassName(p2)
+
+);
